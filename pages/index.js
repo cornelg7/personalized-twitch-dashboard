488ffca79e206215dd7ac7fc5abcfc486b3fb26e
Cornel <cornelg7@yahoo.com>
2021-08-10 12:19:09 +0300

    Added link to local twitch api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,46 @@
 // Main entry point of your app
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React, { useState } from 'react';
 
 const Home = () => {
+  // State
+  const [channels, setChannels] = useState([]);
+
+  // Actions
+  const addChannel = event => {
+    event.preventDefault();
+    const { value } = event.target.elements.name;
+    if (value) {
+      const path = `${window.location.origin}`;
+      const request_data = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data: value })
+      }
+      
+      fetch(`${path}/api/twitch`, request_data).then((response) => {
+        return response.json();
+      }).then(response => {
+        console.log('From server: ', response.data);
+        setChannels(prevState => [...prevState, value]);
+      });
+    }
+    event.target.elements.name.value = '';
+  };
+
+  // Render methods
+  const renderForm = () => (
+    <div className={styles.formContainer}>
+      <form onSubmit={addChannel}>
+        <input id="name" placeholder="Twitch Channel name" type="text" required></input>
+        <button type="submit">Add Channel</button>
+      </form>
+    </div>
+  )
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,8 +48,8 @@ const Home = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className={styles.inputContainer}>
-        <h1>Welcome to the Personalized Twitch Dashboard!! 🎈🎉</h1>
-        <h2>{process.env.DEMO_SECRET}</h2>
+        {renderForm()}
+        <div>{channels.join(', ')}</div>
       </div>
     </div>
   )
