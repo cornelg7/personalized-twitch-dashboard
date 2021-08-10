7e87ab2c57acb06fb2c90f2fdd2b1d4251b46055
Cornel <cornelg7@yahoo.com>
2021-08-10 12:20:26 +0300

    Testing environment variables

--- pages/api/twitch.js
+++ pages/api/twitch.js
@@ -3,7 +3,7 @@ export default async (req, res) => {
   try {
     if (req.method === 'POST') {
       const { data } = req.body;
-      res.status(200).json({ data });
+      res.status(200).json({ data: process.env.DEMO_SECRET });
     }
   } catch (error) {
     concole.warn(error.message);
